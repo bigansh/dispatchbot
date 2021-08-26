3245281d7ebe5ef5cbf5bacd40938dbc24d3c8e6
bigansh_ <anshpushti2001@gmail.com>
2021-08-26 22:45:23 +0530

    add ph badge fe

--- frontend/assets/scripts/index.js
+++ frontend/assets/scripts/index.js
@@ -1,10 +1,13 @@
-const modalButton = document.getElementById('modal-button')
+const modalButton = document.getElementById('modal-button'),
+	phButton = document.getElementById('ph-button')
 
 let modalCount = 0
 
 window.onscroll = () => {
 	if (document.documentElement.scrollTop > 1000 && modalCount === 0) {
 		modalButton.click()
+
+		phButton.classList.add('d-none')
 
 		modalCount++
 	}
